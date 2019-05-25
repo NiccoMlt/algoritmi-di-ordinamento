759e887877c0bf9a12d18fca6bc827295a73c4f6
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-05-25 12:58:27 +0200

    feat: aggiunto Selection Sort

---
+++ src/selection.cpp
@@ -0,0 +1,20 @@
+void SelectionSort(int vet[], int dim) {
+  int tmp;
+
+  for (int i = 0; i < dim - 1; i++) {
+    // Partendo dal primo elemento del sottovettore non ordinato ...
+    int min = i;
+
+    // ... si procede con la ricerca del minimo ...
+    for (int j = i + 1; j < dim; j++) {
+      if (vet[j] < vet[min]) {
+        min = j;
+      }
+    }
+
+    // ... e lo si posiziona alla del sottovettore ordinato
+    tmp = vet[min];
+    vet[min] = vet[i];
+    vet[i] = tmp;
+  }
+}
